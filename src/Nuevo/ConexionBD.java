ad45ce3c4bf03fd933fbf2ec061ded7ffc102b63
Venecio <Venecio@DESKTOP-19H13S3>
2020-11-20 14:31:40 -0300

    1.0

---
+++ src/Nuevo/ConexionBD.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Nuevo;
+
+import java.sql.*;
+
+public class ConexionBD {
+ Connection conexion;
+
+	public Connection conexion(){
+	try{
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		conexion = DriverManager.getConnection("jdbc:mysql://localhost/ark_breeding","Venecio","2277");
+		System.out.println("Conectado :D");
+		}catch(ClassNotFoundException | SQLException e){
+			System.out.println(e.getMessage());	
+			}return conexion;
+	}
+			Statement createStatment(){
+			throw new UnsupportedOperationException("No soportado");
+
+
+                        }
+}
